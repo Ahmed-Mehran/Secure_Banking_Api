27ef5addacaba356809c2bfb3c13fe9178258425
Mehran Dar <mehranahmed22@gmail.com>
2026-01-04 12:11:53 +0530

    Feat: started working on User model, worked on Security Questions Part -- user_auth -> models.py

--- core_apps/user_auth/models.py
+++ core_apps/user_auth/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+## Now as we have created the User model manager, we can now define the Custom User Model
+import uuid
+from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
+from django.utils.translation import gettext_lazy
+
+from .emails import send_account_locked_email
+from .Managers import UserManager
+
+
+class User(AbstractUser):
+    
+    class SecurityQuestions(models.TextChoices):
+        MAIDEN_NAME = (
+            "maiden_name",
+            gettext_lazy("What is your mother's maiden name?"),
+        )
+        FAVORITE_COLOR = (
+            "favorite_color",
+            gettext_lazy("What is your favorite color?"),
+        )
+        BIRTH_CITY = (
+            "birth_city", 
+            gettext_lazy("What is the city where you were born?")
+        )
+        CHILDHOOD_FRIEND = (
+            "childhood_friend",
+            gettext_lazy("What is the name of your childhood best friend?"),
+        )
+    
+    
