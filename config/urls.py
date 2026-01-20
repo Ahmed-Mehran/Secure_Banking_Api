37738fca4b80c32fae38e8e53d6e8c054a873d15
Mehran Dar <mehranahmed22@gmail.com>
2026-01-20 12:09:04 +0530

    Feat: Added the urls for DRF spectacular and also some Django Admin customization, all in urls.py

--- config/urls.py
+++ config/urls.py
@@ -25,8 +25,34 @@ from django.urls import path
 # ]
 ## Can do the above as well but the above is not flexible as settings could be from production as well, so we do the below
 
+### After configuring the settings of DRF spectacular in base.py, we can now add drf spectacular URLS to this main URLS.py file.
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+
 from django.conf import settings
 urlpatterns = [
     path(settings.ADMIN_URL, admin.site.urls),
+    
+    ## These url for DRF spectactular are by default provided
+    path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"), 
+    
+    path("api/v1/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    
+    path("api/v1/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc")
 ]
+
+
+
+
+## The lines below are purely Django Admin customization, not relared to DRF spectacular:
+## site_header → The big title at the top of the admin panel
+## site_title → The browser tab title
+## index_title → The heading on the admin dashboard home page
+admin.site.site_header = "NextGen Bank Admin"
+admin.site.site_title = "NextGen Bank Admin Portal"
+admin.site.index_title = "Welcome to NextGen Bank Admin Portal"
+#### -- CONFUSION: But why the above in the urls.py and not admin.py if they are admin related
+#### -- ANSWER: Those lines are often placed near urls.py (or project setup code) simply because the admin site is initialized and loaded when URLs are configured. At that point, admin.site is guaranteed to
+#               exist, so setting site_header, site_title, and index_title works safely. Conceptually, they belong to the admin, not to URLs — and yes, putting them in admin.py is also perfectly correct. Many
+#               developers prefer urls.py or a central config file just to keep all global project-level customizations in one place, especially things that affect how the admin site looks when accessed via its URL.
+#               In short: They’re in urls.py for convenience and visibility during project setup, not because they are URL-related.
 
