d1ec496a7f494903ae3f4ebc15091cc5b2f5457b
Mehran Dar <mehranahmed22@gmail.com>
2026-07-28 14:50:10 +0530

    Feat : added signals for automatic profile creation object from user object creation

--- core_apps/user_profile/apps.py
+++ core_apps/user_profile/apps.py
@@ -7,3 +7,7 @@ class UserProfileConfig(AppConfig):
     name = "core_apps.user_profile"
     verbose_name = _("User Profile")
     
+    
+    def ready(self) -> None:
+        import core_apps.user_profile.signals        
+    
