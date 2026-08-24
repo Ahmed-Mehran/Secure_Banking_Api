0a1627039c01c617d59507c4656c8a9566ba41bc
Mehran Dar <mehranahmed22@gmail.com>
2026-08-24 19:25:01 +0530

    Feat: modified/extended the custom djoser login endpoint to generate TOKENS after OTP(email/password handled DJOSER)

--- core_apps/user_auth/views.py
+++ core_apps/user_auth/views.py
@@ -42,3 +42,64 @@ def set_auth_cookies(
     logged_in_cookie_settings = cookie_settings.copy()
     logged_in_cookie_settings["httponly"] = False
     response.set_cookie("logged_in", "true", **logged_in_cookie_settings)
+
+
+class CustomTokenCreateView(TokenCreateView):
+    def _action(self, serializer):
+        user = serializer.user
+        if user.is_locked_out:
+            return Response(
+                {
+                    "error": f"Account is locked due to multiple failed login attempts. Please "
+                    f"try again after {settings.LOCKOUT_DURATION.total_seconds() / 60} minutes. ",
+                },
+                status=status.HTTP_403_FORBIDDEN,
+            )
+        user.reset_failed_login_attempts()
+
+        otp = generate_otp()
+        user.set_otp(otp)
+        send_otp_email(user.email, otp)
+
+        logger.info(f"OTP sent for login to user: {user.email}")
+
+        return Response(
+            {
+                "success": "OTP sent to your email",
+                "email": user.email,
+            },
+            status=status.HTTP_200_OK,
+        )
+
+    def post(self, request: Request, *args: Any, **kwargs: Any) -> Response:
+        serializer = self.get_serializer(data=request.data)
+
+        try:
+            serializer.is_valid(raise_exception=True)
+        except Exception:
+            email = request.data.get("email")
+            user = User.objects.filter(email=email).first()
+            if user:
+                user.handle_failed_login_attempts()
+                failed_attempts = user.failed_login_attempts
+                logger.error(
+                    f"Failed login attempts: {failed_attempts}  for user: {email}"
+                )
+                if failed_attempts >= settings.LOGIN_ATTEMPTS:
+                    return Response(
+                        {
+                            "error": f"You have exceeded the maximum number of login attempts. "
+                            f"Your account has been locked for "
+                            f"{settings.LOCKOUT_DURATION.total_seconds() / 60} minutes. "
+                            f"An email has been sent to you with further instructions",
+                        },
+                        status=status.HTTP_403_FORBIDDEN,
+                    )
+            else:
+                logger.error(f"Failed login attempt for non-existent user: {email}")
+
+            return Response(
+                {"error": "Your Login Credentials are not correct"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        return self._action(serializer)
